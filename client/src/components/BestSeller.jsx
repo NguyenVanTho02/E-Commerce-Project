ce283ae82cba67bbe17693ac525228806b68915d
ThoNguyen <hatigr.th@gmail.com>
2023-06-11 16:11:45 +0700

    Product

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -20,26 +20,35 @@ const BestSeller = () => {
     const [bestSellers, setBestSellers] = useState(null);
     const [newProducts, setNewProducts] = useState(null);
     const [activedTab, setActivedTab] = useState(1);
+    const [products, setProducts] = useState(null);
 
     const fetchProducts = async () => {
         const response = await Promise.all([
             apiGetProducts({ sort: "-sold" }),
             apiGetProducts({ sort: "-createAt" }),
         ]);
-        if (response[0]?.success) setBestSellers(response[0].products);
+        if (response[0]?.success) {
+            setBestSellers(response[0].products);
+            setProducts(response[0].products);
+        }
         if (response[1]?.success) setNewProducts(response[1].products);
     };
 
     useEffect(() => {
         fetchProducts();
     }, []);
+
+    useEffect(() => {
+        if (activedTab === 1) setProducts(bestSellers);
+        if (activedTab === 2) setProducts(newProducts);
+    }, [activedTab]);
     return (
         <div>
-            <div className="flex text-[20px] gap-8 pb-4 border-b-2 border-main">
+            <div className="flex text-[20px] ml-[-32px]">
                 {tabs.map((el) => (
                     <span
                         key={el.id}
-                        className={`font-semibold capitalize border-r cursor-pointer text-gray-400 ${
+                        className={`font-semibold capitalize px-8 border-r cursor-pointer text-gray-400 ${
                             activedTab === el.id ? "text-gray-900" : ""
                         }`}
                         onClick={() => setActivedTab(el.id)}
@@ -48,10 +57,14 @@ const BestSeller = () => {
                     </span>
                 ))}
             </div>
-            <div className="mt-4">
+            <div className="mt-4 mx-[-10px] border-t-2 border-main pt-4 ">
                 <Slider {...settings}>
-                    {bestSellers?.map((el) => (
-                        <Product key={el.id} productData={el} />
+                    {products?.map((el) => (
+                        <Product
+                            key={el.id}
+                            productData={el}
+                            isNew={activedTab === 1 ? false : true}
+                        />
                     ))}
                 </Slider>
             </div>
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { formatMoney } from "../ultils/helpers";
+import label from "../assets/new.png";
+import trending from "../assets/trending.png";
 
-const Product = ({ productData }) => {
+const Product = ({ productData, isNew }) => {
     return (
-        <div className="w-1/3">
-            <img
-                src={productData?.images[0] || ""}
-                alt=""
-                className="w-full object-contain"
-            />
+        <div className="w-full text-base px-[10px]">
+            <div className="w-full border p-[15px] flex flex-col items-center">
+                <div className="w-full relative">
+                    <img
+                        src={
+                            productData?.thumb ||
+                            "https://2.bp.blogspot.com/-MowVHfLkoZU/VhgIRyPbIoI/AAAAAAAATtI/fHk-j_MYUBs/s640/placeholder-image.jpg"
+                        }
+                        alt=""
+                        className="w-[243px] h-[243px] object-cover"
+                    />
+                    <img
+                        src={isNew ? label : trending}
+                        alt=""
+                        className={`absolute w-[100px] h-[35px] top-[0] right-[0] object-cover`}
+                    />
+                </div>
+                <div className="flex flex-col gap-1 mt-[15px] items-start w-full">
+                    <span className="line-clamp-1">{productData?.title}</span>
+                    <span>{`${formatMoney(productData?.price)} VND`}</span>
+                </div>
+            </div>
         </div>
     );
 };
